17bf94a4fa2d33040e6b6a7cff7b04c2f16628f3
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2023-07-18 03:12:49 +0700

    add vite plugin for replace dep in devmode

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,32 @@
 import { sveltekit } from '@sveltejs/kit/vite'
+import fs from 'fs'
 import { defineConfig } from 'vitest/config'
 import { sitemapPlugin } from './src/lib'
 
 export default defineConfig({
-	plugins: [sveltekit(), sitemapPlugin()],
+	plugins: [sveltekit(), sitemapPlugin(), (() => {
+		const sitemapRoute = './src/sitemap.ts'
+		const update = () => {
+			const body = fs.readFileSync(sitemapRoute).toString().replace('@binsarjr/sveltekit-sitemap', '$lib')
+			fs.writeFileSync(sitemapRoute, body)
+
+		}
+
+		return {
+			name: 'replace-dependencies-in-sitemap.ts',
+			configureServer(server) {
+				server.watcher
+					.add([sitemapRoute])
+					.on("add", update)
+					.on("unlink", update)
+					.on("unlinkDir", update)
+			}
+		}
+	})()],
 	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
+		include: [
+			'src/**/*.{test,spec}.{js,ts}',
+			'test/**/*.{test,spec}.{js,ts}'
+		],
 	}
 })
